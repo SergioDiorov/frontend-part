3878a4e76d59af224240786188500ba97ef97612
SergioDiorov <137777587+SergioDiorov@users.noreply.github.com>
2023-08-08 09:30:57 +0300

    feat: add edit and delete functionality to users page (#6)
    * add button functionality of editing and deleting chosen user;
    * add modal windows with editing form and delete confirmation;
    * add new reducer to handle with chosen user

--- src/components/MainContent/CurrentUser/CurrentUserCard/CurrentUserCard.tsx
+++ src/components/MainContent/CurrentUser/CurrentUserCard/CurrentUserCard.tsx
@@ -2,7 +2,14 @@ import style from 'components/MainContent/CurrentUser/CurrentUserCard/CurrentUse
 import userDafaultImage from 'img/assets/avatarProfileUser.png';
 import { UserType } from 'types/types';
 
-export const CurrentUserCard: React.FC<UserType> = ({
+type CurrentUserCardType = Partial<UserType> & {
+  setEditUserMode: (param: boolean) => void;
+  setDeleteUserMode: (param: boolean) => void;
+};
+
+export const CurrentUserCard: React.FC<CurrentUserCardType> = ({
+  setEditUserMode,
+  setDeleteUserMode,
   userName,
   email,
   isAdmin,
@@ -13,8 +20,20 @@ export const CurrentUserCard: React.FC<UserType> = ({
       <p className={style.userName}>{userName}</p>
       <p className={style.email}>{email}</p>
       <p className={style.role}>{isAdmin ? 'admin' : 'user'}</p>
-      <button className={style.editButton}>Edit</button>
-      <button className={style.deleteButton}>Delete</button>
+      <button
+        className={style.editButton}
+        onClick={() => setEditUserMode(true)}
+      >
+        Edit
+      </button>
+      <button
+        className={style.deleteButton}
+        onClick={() => {
+          setDeleteUserMode(true);
+        }}
+      >
+        Delete
+      </button>
     </div>
   );
 };
---
+++ src/components/MainContent/CurrentUser/EditUserModal/EditUserModal.tsx
@@ -0,0 +1,138 @@
+import { Field, Form, Formik } from 'formik';
+import { useDispatch } from 'react-redux';
+import * as Yup from 'yup';
+
+import { changeUserData } from 'redux/current-user-reducer';
+import { AppDispatch } from 'redux/store';
+import { UserType } from 'types/types';
+import style from 'components/MainContent/CurrentUser/EditUserModal/EditUserModal.module.scss';
+import avatarProfileUser from 'img/assets/avatarProfileUser.png';
+
+type EditUserModalPropsType = {
+  setEditUser: (param: boolean) => void;
+  userName?: string;
+  email?: string;
+  isAdmin?: boolean;
+  userId: string;
+};
+
+const UserDataSchema = Yup.object().shape({
+  email: Yup.string()
+    .matches(
+      /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+      'Invalid email address'
+    )
+    .max(30, 'Too Long!')
+    .required('User name is required'),
+  userName: Yup.string().max(30, 'Too Long!').required('User name is required'),
+});
+
+export const EditUserModal: React.FC<EditUserModalPropsType> = ({
+  setEditUser,
+  userName,
+  email,
+  isAdmin,
+  userId,
+}) => {
+  const dispatch = useDispatch<AppDispatch>();
+
+  return (
+    <div
+      className={style.modalContainer}
+      onClick={(e) => e.target === e.currentTarget && setEditUser(false)}
+    >
+      <Formik
+        enableReinitialize={true}
+        initialValues={{
+          file: '',
+          userName: userName,
+          email: email,
+          role: isAdmin,
+        }}
+        validationSchema={UserDataSchema}
+        onSubmit={(values, { resetForm }) => {
+          let userCredentials: Partial<UserType> = {};
+
+          if (values.email !== email) {
+            userCredentials.email = values.email;
+          }
+
+          if (values.role !== isAdmin) {
+            userCredentials.isAdmin = values.role;
+          }
+
+          if (values.userName !== userName) {
+            userCredentials.userName = values.userName;
+          }
+
+          if (!!Object.keys(userCredentials).length) {
+            dispatch(changeUserData({ userId, userCredentials }));
+          }
+
+          resetForm();
+        }}
+      >
+        {({ errors, touched }) => (
+          <Form className={style.formContainer}>
+            <h2 className={style.formTitle}>Edit</h2>
+
+            <div className={style.fieldWrapper}>
+              <label className={style.fileLabel}>
+                <img src={avatarProfileUser} alt='User avatar' />
+                <span>Choose picture</span>
+                <Field type='file' name='file' className={style.fileInput} />
+              </label>
+              {errors.file && touched.file && (
+                <div className={style.fieldError}>{errors.file}</div>
+              )}
+            </div>
+
+            <div className={style.fieldWrapper}>
+              <span>Username</span>
+              <Field
+                type='userName'
+                name='userName'
+                placeholder='User name'
+                className={style.textInput}
+              />
+              {errors.userName && touched.userName && (
+                <div className={style.fieldError}>{errors.userName}</div>
+              )}
+            </div>
+
+            <div className={style.fieldWrapper}>
+              <span>Email</span>
+              <Field
+                type='email'
+                name='email'
+                placeholder='Email'
+                className={style.textInput}
+              />
+              {errors.email && touched.email && (
+                <div className={style.fieldError}>{errors.email}</div>
+              )}
+            </div>
+
+            <label className={style.checkboxContiner}>
+              <span className={style.checkboxTitle}>Role</span>
+              <Field type='checkbox' name='role' />
+              <span className={style.checkboxSpan}>Admin</span>
+            </label>
+
+            <div className={style.formButtonContainer}>
+              <button type='submit' className={style.submitButton}>
+                Save
+              </button>
+              <button
+                onClick={() => setEditUser(false)}
+                className={style.closeButton}
+              >
+                Close
+              </button>
+            </div>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+};
